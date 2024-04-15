d804f9cfc5fc0ce6ed96338b889a8978318f1443
Md.Sharif Mollah <41167908+mdsharifmollah@users.noreply.github.com>
2024-04-15 19:24:25 +0600

    file update

--- js/main.js
+++ js/main.js
@@ -1,78 +1,77 @@
   // read more  one
-$(document).ready(function() {
-  $("#toggle").click(function() {    
-    let elem = $("#toggle").text();
-    if (elem == "Read More") {
-      $("#toggle").text("Read Less");
-      $("#moreText").slideDown();
-    } else {
-      $("#toggle").text("Read More");
-      $("#moreText").slideUp();
-    }
-    
-    
-  });
-});
-// read more  two
-         $(document).ready(function() {
-  $("#toggle2").click(function() {    
-    let elem = $("#toggle2").text();
-    if (elem == "Read More") {
-      $("#toggle2").text("Read Less");
-      $("#moreText2").slideDown();
-    } else {
-      $("#toggle2").text("Read More");
-      $("#moreText2").slideUp();
-    }
-    
-    
-  });
-});
-    // read more  three
   $(document).ready(function() {
-  $("#toggle3").click(function() {    
-    let elem = $("#toggle3").text();
-    if (elem == "Read More") {
-      $("#toggle3").text("Read Less");
-      $("#moreText3").slideDown();
-    } else {
-      $("#toggle3").text("Read More");
-      $("#moreText3").slideUp();
-    }
-    
-    
+      $("#toggle").click(function() {
+          let elem = $("#toggle").text();
+          if (elem == "Read More") {
+              $("#toggle").text("Read Less");
+              $("#moreText").slideDown();
+          } else {
+              $("#toggle").text("Read More");
+              $("#moreText").slideUp();
+          }
+
+
+      });
   });
-});
+  // read more  two
+  $(document).ready(function() {
+      $("#toggle2").click(function() {
+          let elem = $("#toggle2").text();
+          if (elem == "Read More") {
+              $("#toggle2").text("Read Less");
+              $("#moreText2").slideDown();
+          } else {
+              $("#toggle2").text("Read More");
+              $("#moreText2").slideUp();
+          }
 
 
-// menu
+      });
+  });
+  // read more  three
+  $(document).ready(function() {
+      $("#toggle3").click(function() {
+          let elem = $("#toggle3").text();
+          if (elem == "Read More") {
+              $("#toggle3").text("Read Less");
+              $("#moreText3").slideDown();
+          } else {
+              $("#toggle3").text("Read More");
+              $("#moreText3").slideUp();
+          }
 
-  $(document).ready(function(){
-  $('ul li a').click(function(){
-    $('li a').removeClass("active");
-    $(this).addClass("active");
-});
-});
 
-//Scrolling Effect
-$(window).on('scroll', function () {
-  if ($(window).scrollTop()) {
-    $('.header').addClass('black');
-  } else {
-    $('.header').removeClass('black');
+      });
+  });
+
+
+  // menu
+
+  $(document).ready(function() {
+      $('ul li a').click(function() {
+          $('li a').removeClass("active");
+          $(this).addClass("active");
+      });
+  });
+
+  //Scrolling Effect
+  $(window).on('scroll', function() {
+      if ($(window).scrollTop()) {
+          $('.header').addClass('black');
+      } else {
+          $('.header').removeClass('black');
+      }
+  });
+
+  function openDaySchedule(url) {
+      // See all options https://dayschedule.com/widget
+      daySchedule.initPopupWidget({
+          url: url,
+          color: {
+              primary: '#232325',
+              secondary: '#adadad',
+          },
+      });
   }
-});
 
-function openDaySchedule(url) {
-  // See all options https://dayschedule.com/widget
-  daySchedule.initPopupWidget({
-    url: url,
-    color: {
-      primary: '#232325',
-      secondary: '#adadad',
-    },
-  });
-}
-
-new WOW().init();
-
+  new WOW().init();
